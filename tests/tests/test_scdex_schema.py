18194cb73bbee0e4bd89ceb2bf25f3a456a3ac3e
Nate Ferrell <nate@scrthq.com>
2024-11-02 22:30:12 -0500

    feat: #comment updated metaschema and tests

--- tests/tests/test_scdex_schema.py
+++ tests/tests/test_scdex_schema.py
@@ -1,7 +1,7 @@
 import json
 import jsonschema
 import pytest
-from jsonschema import validate, Draft7Validator
+from jsonschema import validate, Draft202012Validator
 from scdex import SCDEX_JSON_SCHEMA, SCDEX_SCHEMA_REPO_ROOT
 
 
@@ -12,7 +12,7 @@ def test_schema_is_resolved():
     assert isinstance(SCDEX_JSON_SCHEMA, dict)
     assert SCDEX_JSON_SCHEMA['title'] == 'SC-DEX Core Schema'
 
-    assert Draft7Validator.check_schema(SCDEX_JSON_SCHEMA) is None
+    assert Draft202012Validator.check_schema(SCDEX_JSON_SCHEMA) is None
 
 
 @pytest.mark.parametrize("path_to_validate", examples_path.glob("valid/*.json"))
@@ -21,7 +21,7 @@ def test_valid_examples(path_to_validate):
     with open(path_to_validate, 'r') as f:
         data = f.read()
     data = json.loads(data)
-    validate(data, SCDEX_JSON_SCHEMA, cls=Draft7Validator)
+    validate(data, SCDEX_JSON_SCHEMA, cls=Draft202012Validator)
 
 
 @pytest.mark.parametrize("path_to_validate", examples_path.glob("invalid/*.json"))
@@ -31,4 +31,4 @@ def test_invalid_examples(path_to_validate):
         data = f.read()
     data = json.loads(data)
     with pytest.raises(jsonschema.exceptions.ValidationError):
-        validate(data, SCDEX_JSON_SCHEMA, cls=Draft7Validator)
+        validate(data, SCDEX_JSON_SCHEMA, cls=Draft202012Validator)
