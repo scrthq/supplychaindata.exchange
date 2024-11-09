34cc36ea094bd96d4769b0a5e594a0913833a28b
Nate Ferrell <nate@scrthq.com>
2024-11-08 18:10:10 -0600

    feat: #comment updated homepage and added docs/spec

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -10,7 +10,7 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Easy to Use?',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
