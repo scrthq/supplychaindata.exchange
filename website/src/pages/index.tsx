a74b3eba357324ca01cdea8ddd2eb26f50b0a245
Nate Ferrell <nate@scrthq.com>
2024-11-10 23:02:57 -0600

    feat: #comment added HeroComponent WIP, updated header

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,6 +6,7 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
+import SCDEXHeroComponent from "../components/SCDEXHeroComponent";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -149,6 +150,7 @@ export default function Home(): JSX.Element {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        {/* <SCDEXHeroComponent /> */}
         <ExpertInterviewGrid />
       </main>
     </Layout>
