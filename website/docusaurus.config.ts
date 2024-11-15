42901108dfafc0047f5958e9b01da97721d25375
Nate Ferrell <nate@scrthq.com>
2024-11-15 00:54:13 -0600

    chore: updated config

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/opensupplyhub/supplychaindata.exchange/tree/main/website/docs/",
         },
         blog: {
           showReadingTime: true,
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/opensupplyhub/supplychaindata.exchange/tree/main/website/",
+            "https://github.com/opensupplyhub/supplychaindata.exchange/tree/main/website/blog/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
