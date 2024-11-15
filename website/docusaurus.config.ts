e709865d84e03f40ee72b7f161c15a96d35161b3
Nate Ferrell <nate@scrthq.com>
2024-11-15 00:45:50 -0600

    feat: updated site title

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -66,9 +66,9 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
+      title: "SC-DEX",
       logo: {
-        alt: "My Site Logo",
+        alt: "SC-DEX Logo",
         src: "img/logo.svg",
       },
       items: [
